bb4c3be72e2b2ce2333139ccae863e294c858849
XZPshaw <zepeng@ualberta.ca>
2021-09-08 19:32:59 -0600

    final version

--- lab1.py
+++ lab1.py
@@ -1,6 +1,20 @@
 import requests
 
-print('The version of requests library is %s'%requests.__version__)
+'''virtualenv step4'''
+print("The viersion of requests library is %s" % requests.__version__)
 
+'''curl step5'''
 homepage = requests.get("http://www.google.com/")
-print("GET Google homepage\n", homepage.text)
+# print("GET Google Homepage\n",homepage)
+
+'''curl step10'''
+code_url = "https://raw.githubusercontent.com/XZPshaw/CMPUT404LAB1/main/lab1.py"
+# code_url = "https://github.com/XZPshaw/CMPUT404LAB1/blob/main/lab1.py"
+code_file = requests.get(code_url, stream=True)
+# print(code_file.content)
+with open("downloaded_lab1_zepeng.py", "wb") as code:
+    code.write(code_file.content)
+
+with open("downloaded_lab1_zepeng.py", "r") as code:
+    for line in code:
+        print(line)
