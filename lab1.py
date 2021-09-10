a567904cfabfcc97cc6070d39fcdb3e3c17c57f4
XZPshaw <zepeng@ualberta.ca>
2021-09-10 14:29:02 -0600

    modified print source,now directly print from github page

--- lab1.py
+++ lab1.py
@@ -11,10 +11,7 @@ homepage = requests.get("http://www.google.com/")
 code_url = "https://raw.githubusercontent.com/XZPshaw/CMPUT404LAB1/main/lab1.py"
 # code_url = "https://github.com/XZPshaw/CMPUT404LAB1/blob/main/lab1.py"
 code_file = requests.get(code_url, stream=True)
-# print(code_file.content)
+
 with open("downloaded_lab1_zepeng.py", "wb") as code:
     code.write(code_file.content)
 
-with open("downloaded_lab1_zepeng.py", "r") as code:
-    for line in code:
-        print(line)
